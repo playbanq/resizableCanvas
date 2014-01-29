e0ae1ff853dc68a9fd30500b7d1a03ca380083ed
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-28 19:47:24 -0500

    Removed the #colorCanvas function and used CSS backgroundColor property.

--- examples/coloredCanvas.js
+++ examples/coloredCanvas.js
@@ -3,17 +3,12 @@ var canvas = document.getElementById('resizableCanvas'),
 
 ResizableCanvas.extend(canvas);
 canvas.setSize();
-colorCanvas(128, Math.floor(canvas.width/10), Math.floor(canvas.height/5));
+canvas.style.backgroundColor = 'rgb(128, ' + Math.floor(canvas.width/10) + 
+                                ', ' + Math.floor(canvas.height/5) + ')';
 
 // Handle canvas/window resizing
-canvas.onResize(function (type, width, height) {
+canvas.onResize(function (width, height, type) {
     var green = Math.floor(width/10),
         blue = Math.floor(height/5);
-    colorCanvas(128, green, blue);
+    canvas.style.backgroundColor = 'rgb(128, ' + green + ', ' + blue + ')';
 });
-
-// Change canvas background color
-function colorCanvas(red, green, blue) {
-    context.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-}
